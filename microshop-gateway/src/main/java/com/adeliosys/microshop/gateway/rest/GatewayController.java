8adb600da5900b6fa2f59489e7ffbfe8ac585b48
Florian Beaufumé <fbeaufume@gmail.com>
2019-09-15 15:12:11 +0200

    Removed unused import

--- microshop-gateway/src/main/java/com/adeliosys/microshop/gateway/rest/GatewayController.java
+++ microshop-gateway/src/main/java/com/adeliosys/microshop/gateway/rest/GatewayController.java
@@ -1,7 +1,6 @@
 package com.adeliosys.microshop.gateway.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
